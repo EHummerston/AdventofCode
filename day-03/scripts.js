044c5246f9f779dd104383a5d17f76824965451b
Edward Hummerston <ed.hummerston@gmail.com>
2017-12-03 22:58:41 +1100

    day 3 part 1 completed messilly

---
+++ day-03/scripts.js
@@ -0,0 +1,57 @@
+const dayThree = {
+
+    // DAY 1-1
+    1: (input) => {
+        const r = ~~(Math.ceil(Math.sqrt(input)) / 2) + 1;
+        const d = (r * 2) - 1;
+        const quarter = (r-1)*2;
+        const index = input - Math.pow(d - 2, 2);
+        const c = Math.abs(((index) % (quarter)) - (r-1));
+        const steps = r + c - 1;
+        return steps;
+    },
+
+    // DAY 1-2
+    2: (input) => {
+        const r = ~~(Math.ceil(Math.sqrt(input)) / 2) + 1;
+        const d = (r * 2) - 1;
+        const quarter = (r-1)*2;
+        const index = input - Math.pow(d - 2, 2);
+        const c = Math.abs(((index) % (quarter)) - (r-1));
+        const steps = r + c - 1;
+        return steps;
+    }
+
+};
+
+$('.solve').click(function () {
+    const button = $(this);
+    if (!button.prop('disabled')) {
+        button.prop('disabled', true);
+        const solutionPart = button.closest('.advent-problem').data('problem');
+        const code = button.siblings('code, pre');
+        code.text(dayThree[solutionPart](code.text()));
+        code.removeClass('number');
+    }
+});
+
+const day3Input = 368078;
+
+console.log("\tDay 3-1 test data:");
+console.log(dayThree[1](1));
+console.log(dayThree[1](12));
+console.log(dayThree[1](23));
+console.log(dayThree[1](1024));
+
+console.log("\tDay 3-1 answer:");
+console.log(dayThree[1](day3Input));
+
+console.log("\tDay 3-2 test data:");
+console.log(dayThree[2](1));
+console.log(dayThree[2](2));
+console.log(dayThree[2](3));
+console.log(dayThree[2](4));
+console.log(dayThree[2](5));
+
+console.log("\tDay 3-2 answer:");
+console.log(dayThree[2](day3Input));
